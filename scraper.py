ab1c60579f30597a648d7437ee5a9126ba268111
Hasnain Khan <hasnnain.khan@theowlsense.com>
2022-04-27 23:38:56 +0500

    changes

--- scraper.py
+++ scraper.py
@@ -81,93 +81,102 @@ def Scrap():
     time.sleep(3)
     driver.get("https://www.skroutz.gr")
     # driver.find_element(By.ID, "accept-all").click()
-    for product_id, product_name in enumerate(p_name):
-        search_bar = WebDriverWait(driver, 180).until(
-            EC.presence_of_element_located((By.XPATH, '//input[@id="search-bar-input"]')))
-        search_bar.clear()
-        search_bar.send_keys(product_name)
-        search_bar.send_keys(Keys.RETURN)
-        try:
-            link = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, "//a[@class='js-sku-link']")))
-        except TimeoutException:
-            # time.sleep(1)
-            continue
-        try:
-            page_title = driver.find_element(By.XPATH, "//h1[@class='page-title']")
-            # time.sleep(1)
-            continue
-        except NoSuchElementException:
-            # time.sleep(1)
-            driver.execute_script("arguments[0].click();", link)
+    ids = 1
+    try:
+        for product_id, product_name in enumerate(p_name):
+            search_bar = WebDriverWait(driver, 180).until(
+                EC.presence_of_element_located((By.XPATH, '//input[@id="search-bar-input"]')))
+            search_bar.clear()
+            search_bar.send_keys(product_name)
+            search_bar.send_keys(Keys.RETURN)
             try:
-                product_cards = WebDriverWait(driver, 10).until(
-                    EC.presence_of_all_elements_located((By.XPATH, '//li[@class="card js-product-card"]')))
-                # time.sleep(1)
+                link = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.XPATH, "//a[@class='js-sku-link']")))
             except TimeoutException:
                 # time.sleep(1)
                 continue
-            num_cards = len(product_cards)
-            i = 0
-            flag = 0
-            for product_card in product_cards:
-                ActionChains(driver).move_to_element(product_card).perform()
+            try:
+                page_title = driver.find_element(By.XPATH, "//h1[@class='page-title']")
+                # time.sleep(1)
+                continue
+            except NoSuchElementException:
+                # time.sleep(1)
+                driver.execute_script("arguments[0].click();", link)
                 try:
-                    ignored_exceptions = (NoSuchElementException, StaleElementReferenceException,)
-                    wait = WebDriverWait(driver, 10, ignored_exceptions=ignored_exceptions)
-                    store_name = wait.until(lambda d: product_card.find_element(By.CLASS_NAME, "shop-name")).text
-                    if store_name == mystore:
-                        flag = 1
-                    if flag == 0 and i >= 4:
-                        i = i + 1
-                        continue
-                    price = wait.until(lambda d: product_card.find_element(By.CLASS_NAME, "dominant-price")).text
-                    shipping_prices = wait.until(
-                        lambda d: product_card.find_elements(By.CSS_SELECTOR, ".extra-cost.cf>em"))
-                    shipping = shipping_prices[0].text
-                    delivery = shipping_prices[1].text
-                    deliver_time = wait.until(
-                        lambda d: product_card.find_element(By.CSS_SELECTOR, "span.availability")).text
-                except:
+                    product_cards = WebDriverWait(driver, 10).until(
+                        EC.presence_of_all_elements_located((By.XPATH, '//li[@class="card js-product-card"]')))
+                    # time.sleep(1)
+                except TimeoutException:
+                    # time.sleep(1)
                     continue
-                data_ = [product_id, product_name, store_name, str(i + 1), num_cards, price, shipping, delivery,
-                         deliver_time]
-                df = pd.DataFrame(data=data_,
-                                  index=["id", "product_name", "storename", "place", "totalnum", "price",
-                                         "shipping", "delivery", "availability"]).T
-                df.to_csv("compete.csv", mode='a', header=not (os.path.isfile("compete.csv")), index=False)
-                i = i + 1
-                if i >= 5:
-                    if flag == 1:
-                        break
-    driver.quit()
-    print("Scraping completed")
+                num_cards = len(product_cards)
+                i = 0
+                for product_card in product_cards:
+                    ActionChains(driver).move_to_element(product_card).perform()
+                    try:
+                        ignored_exceptions = (NoSuchElementException, StaleElementReferenceException,)
+                        wait = WebDriverWait(driver, 10, ignored_exceptions=ignored_exceptions)
+                        store_name = wait.until(lambda d: product_card.find_element(By.CLASS_NAME, "shop-name")).text
+                        if store_name == mystore:
+                            # flag = 1
+                            # if flag == 0 and i >= 4:
+                            #     i = i + 1
+                            continue
+                        price = wait.until(lambda d: product_card.find_element(By.CLASS_NAME, "dominant-price")).text
+                        shipping_prices = wait.until(
+                            lambda d: product_card.find_elements(By.CSS_SELECTOR, ".extra-cost.cf>em"))
+                        shipping = shipping_prices[0].text
+                        delivery = shipping_prices[1].text
+                        deliver_time = wait.until(
+                            lambda d: product_card.find_element(By.CSS_SELECTOR, "span.availability")).text
+                    except:
+                        continue
+                    data_ = [ids, product_id + 1, store_name, str(i + 1), num_cards, price, shipping,
+                             delivery,
+                             deliver_time]
+                    df = pd.DataFrame(data=data_,
+                                      index=["id", "product", "storename", "place", "totalnum", "price",
+                                             "shipping", "delivery", "availability"]).T
+                    df.to_csv("compete.csv", mode='a', header=not (os.path.isfile("compete.csv")), index=False)
+                    i = i + 1
+                    ids = ids + 1
+        print("Scraping completed")
+    except:
+        pass
+    finally:
+        driver.quit()
 
 
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     ReadXml()
-#     Scrap()
-
-# f = open('compete.csv')
-df = pd.read_csv('compete.csv')
+def xml():
+    f = open('compete.csv')
+    df = pd.read_csv('compete.csv')
+    xml_ = ('\n'.join(df.apply(convert_row, axis=1)))
+    xml_ = str(("<webstore>", xml_, "</webstore>"))
+    with open("compete.xml", "w") as f:
+        f.write("<webstore>" + '\n'.join(df.apply(convert_row, axis=1)) + "</webstore>")
 
 
 def convert_row(row):
     return """<competitor>
     <id>%s</id>
-   <type>%s</type>
-   <format>%s</format>
-   <year>%s</year>
-   <rating>%s</rating>
-   <stars>%s</stars>
-   <description>%s</description>
-</competitor>""" % (
-        row.product_id, row.product_name, row.storename, row.place, row.totalnum, row.price, row.shipping)
+   <product>%s</product>
+   <storename>%s</storename>
+   <place>%s</place>
+   <totalnum>%s</totalnum>
+   <price>%s</price>
+   <shipping>%s</shipping>
+    <delivery>%s</delivery>
+    <availability>%s</availability>
+    
+   </competitor>""" % (
+        row.id, row[1], row.storename, row.place, row.totalnum, row.price, row.shipping, row.delivery,
+        row.availability)
 
 
-xml_ = ('\n'.join(df.apply(convert_row, axis=1)))
-xml_ = str(("<webstore>", xml_, "</webstore>"))
-with open("test.xml", "w") as f:
-    f.write("<webstore>"+'\n'.join(df.apply(convert_row, axis=1))+"</webstore>")
+try:
+    ReadXml()
+    Scrap()
+except Exception as e:
+    print(e)
 
+xml()
